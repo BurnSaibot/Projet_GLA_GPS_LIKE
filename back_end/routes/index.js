371bea7e04ea26b42d92498ecd900629052a49e6
ShenjinLYU <shenjin_lyu@ens.univ-artois.fr>
2019-05-11 16:08:16 +0200

    connexion-inscription

--- back_end/routes/index.js
+++ back_end/routes/index.js
@@ -9,6 +9,46 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'GPS' });
 });
 
+/* inscrire */
+
+router.route('/inscrire').get(function(req, res) {
+  res.render('inscrire', { title: 'inscrire' });
+});
+router.post(('/register'), function(req, res) {
+  var password=req.body.password;
+  var nom=req.body.nom;
+  var prenom=req.body.prenom;
+  var mail = req.body.mail;
+	
+	user.findOne({mail:mail},function(err,result){
+		if(err){
+			console.log(err);
+		}
+		else if(result){
+      console.log("compte existe");
+      res.render('inscrire', { title: 'login' });
+		}
+		else{
+			user.create({
+				mail:mail,
+        password:password,
+        nom:nom,
+        prenom:prenom
+			},function(err,doc){
+				if(err){
+					console.log(err);
+				}
+				else{
+          console.log("compte est cree");
+          res.render('register', { title: 'register' });
+				}
+			})
+		}
+	});
+});
+
+
+
 /* login */
 router.get('/login',function(req, res) {
   res.render('login', {title: 'login'});
@@ -19,9 +59,9 @@ router.get('/logout', function(req, res) {
   res.render('logout', { title: 'logout' });
 });
 
-/*hompage*/
+/* hompage */
 router.post('/homepage', function(req, res) {
-var query_doc = {mail: req.body.mail, mdpHashe: req.body.password};
+var query_doc = {mail: req.body.mail, password: req.body.password};
 (function(){
     user.count(query_doc, function(err, doc){
         if(doc == 1){
@@ -29,10 +69,10 @@ var query_doc = {mail: req.body.mail, mdpHashe: req.body.password};
             res.render('homepage', { title: 'homepage' });
         }else{
             console.log(query_doc.userid + ": login failed in " + new Date());
-            res.redirect('/');
+            res.redirect('login');
         }
     });
-})(query_doc);
+  })(query_doc);
 });
 
 
