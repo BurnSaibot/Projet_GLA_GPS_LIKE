b40ea5e86c9f9d41aa195616f91deef2cbeacac7
Unknown <eric.cerveau@hotmail.fr>
2019-05-13 16:37:55 +0200

    added route & somme common function

---
+++ back_end/controllers/utils.js
@@ -0,0 +1,17 @@
+var sendError = exports.response.sendError = function (res, error, code) {
+
+    if (code === undefined) {
+      code = 500;
+    }
+  
+    res.status(code).json({
+      error: error
+    });
+  
+};
+  
+var sendSuccess = exports.response.sendSuccess = function (res, success) {
+    res.status(200).json({
+      success: success
+    });
+};
