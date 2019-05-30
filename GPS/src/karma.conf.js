f6691c49e8912dfcba4de4b46279fefa461755a5
Juliette Malassé <jmalasse@gmail.com>
2019-05-30 16:00:10 +0200

    Update conf files

--- GPS/src/karma.conf.js
+++ GPS/src/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/GPS'),
+      dir: require('path').join(__dirname, '../coverage/GPS-LIKE'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
